5265c5221e3eee1f6fa90f2215217d25eba05412
Paulo Keller <paulo.vinicius.keller@gmail.com>
2019-07-09 16:57:28 -0300

    initial commit

---
+++ Downtown-Game/scripts/NPC.gd
@@ -0,0 +1,16 @@
+extends "res://scripts/Character.gd"
+
+var textures = ['mage_f.png', 'mage_m.png',
+				'healer_f.png', 'healer_m.png',
+				'townfolk1_f.png', 'townfolk1_m.png']
+func _ready():
+	randomize()
+	var texture = textures[randi() % textures.size()]
+	texture = load("res://res/rpgsprites1/%s" % texture)
+	$Sprite.texture = texture
+	facing = moves.keys()[randi() % 4]
+	
+func _process(delta):
+	if can_move:
+		if not move(facing) or randi() % 10 > 5:
+			facing = moves.keys()[randi() % 4]
