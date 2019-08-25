29133db9779a2d7af78d64faab2c1abb3fdc890f
PauloKeller <paulo.vinicius.keller@gmail.com>
2019-08-25 20:38:16 -0300

    unreal course added

---
+++ Unreal/The-Unreal-Engine-Developer-Course/Section_01/Intermediate/ProjectFiles/cmake-headers-project.cmake
@@ -0,0 +1,5 @@
+set(PROJECT_HEADER_FILES 
+	"/home/paulo/Workspace/Game-Development/Unreal/The-Unreal-Engine-Developer-Course/Section_01/Source/Section_01/Section_01.h"
+	"/home/paulo/Workspace/Game-Development/Unreal/The-Unreal-Engine-Developer-Course/Section_01/Source/Section_01/Section_01GameModeBase.h"
+ )
+
---
+++ Unreal/The-Unreal-Engine-Developer-Course/Section_01/Intermediate/ProjectFiles/cmake-sources-project.cmake
@@ -0,0 +1,5 @@
+set(PROJECT_SOURCE_FILES 
+	"/home/paulo/Workspace/Game-Development/Unreal/The-Unreal-Engine-Developer-Course/Section_01/Source/Section_01/Section_01.cpp"
+	"/home/paulo/Workspace/Game-Development/Unreal/The-Unreal-Engine-Developer-Course/Section_01/Source/Section_01/Section_01GameModeBase.cpp"
+ )
+
