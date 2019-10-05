2a481f4f23bc2a62b141e02abd22521d17041481
PauloKeller <paulo.vinicius.keller@gmail.com>
2019-10-05 18:09:32 -0300

    open the door

--- Unreal/The-Unreal-Engine-Developer-Course/BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ Unreal/The-Unreal-Engine-Developer-Course/BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -6,7 +6,7 @@
 
 
 // Sets default values for this component's properties
-UOpenDoor::UOpenDoor()
+UOpenDoor::UOpenDoor() 
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -15,26 +15,28 @@ UOpenDoor::UOpenDoor()
 	// ...
 }
 
-
 // Called when the game starts
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	AActor* Owner = GetOwner();
-	FRotator NewRotation = FRotator(0.f, -60.f, 0.f);
-	Owner -> SetActorRotation(NewRotation);
-
-	// ...
 	
 }
 
+void UOpenDoor::OpenDoor() {
+	AActor* Owner = GetOwner();
+	FRotator NewRotation = FRotator(0.f, -60.f, 0.f);
+	Owner -> SetActorRotation(NewRotation);
+}
 
 // Called every frame
 void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
+	// Poll the Trigger Volume
+	// If the ActorThatOpens is in the volume
+	if (PressurePlate -> IsOverlappingActor(ActorThatOpens)) {
+		OpenDoor();
+	}
 }
 
