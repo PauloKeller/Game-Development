bf363612bdc22e64dc8b0bad30f2989c66ff1439
Paulo Keller <paulo.vinicius.keller@gmail.com>
2023-08-07 22:59:50 -0300

    New project completed

---
+++ Unity/SnowBoarder/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float torqueAmount = 10f;
+    [SerializeField]
+    float boostSpeed = 30f;
+    [SerializeField]
+    float baseSpeed = 20f;
+
+
+    Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector;
+    bool canMove = true;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector = FindObjectOfType<SurfaceEffector2D>();
+    }
+
+    void Update()
+    {
+        if (canMove)
+        {
+            RotatePlayer();
+            RespondToBoost();
+        }
+    }
+
+    public void DisableControls() 
+    { 
+        canMove = false;
+    }
+
+    void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector.speed = boostSpeed;
+        }
+        else 
+        {
+            surfaceEffector.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb2d.AddTorque(torqueAmount);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb2d.AddTorque(-torqueAmount);
+        }
+    }
+}
