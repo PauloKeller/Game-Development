559c71559b58b07135ec9da415f4429f386d48db
Paulo Keller <paulo.vinicius.keller@gmail.com>
2024-02-08 22:37:32 -0300

    drop system

---
+++ Unity/RPG Udemy Course/Assets/Scripts/ItemDrop.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDrop : MonoBehaviour
+{
+    [SerializeField] private int possibleItemDrop;
+    [SerializeField] private ItemData[] possibleDrop;
+    private List<ItemData> dropList = new List<ItemData>();
+
+    [SerializeField] private GameObject dropPrefab;
+    [SerializeField] private ItemData item;
+
+    public void GenerateDrop()
+    {
+        for (int i = 0; i < possibleDrop.Length; i++)
+        {
+            if (Random.Range(0, 100) <= possibleDrop[i].dropChance)
+                dropList.Add(possibleDrop[i]);
+        }
+
+        for (int i = 0; i < possibleItemDrop; i++)
+        {
+            int dropIndex = Random.Range(0, dropList.Count - 1);
+            Debug.Log($"drop index {dropIndex}");
+
+            ItemData randomItem = dropList[dropIndex];
+
+            dropList.Remove(randomItem);
+            DropItem(randomItem);
+        }
+    }
+
+    public void DropItem(ItemData _itemData)
+    {
+        GameObject newDrop = Instantiate(dropPrefab, transform.position, Quaternion.identity);
+        Vector2 randomVelocity = new Vector2(Random.Range(-5, 5), Random.Range(15, 20));
+
+        newDrop.GetComponent<ItemObject>().SetupItem(_itemData, randomVelocity);
+    }
+}
